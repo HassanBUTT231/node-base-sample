4569a456222f0c4c5ec4f2fbc975776755857f10
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-28 10:48:23 +0500

    Create code_deploy_agent.sh

---
+++ code_deploy_agent.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install ruby-full
+sudo apt install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo systemctl start codedeploy-agent
+sudo systemctl enable codedeploy-agent
