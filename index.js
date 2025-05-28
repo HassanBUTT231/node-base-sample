b779465ac69fefd2d4c835f47eb2054d3888927c
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-28 11:41:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+
+const port = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js App deployed on EC2 using CodeDeploy!');
+  res.send('Hello from Node.js App deployed on EC2 using CodeDeploy and Docker!');
 });
 
-app.listen(port, () => {
+// Start the server and listen on all network interfaces
+app.listen(port, '0.0.0.0', () => {
   console.log(`App listening on port ${port}`);
 });
