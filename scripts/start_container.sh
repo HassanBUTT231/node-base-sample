e88eb2f807cfda3c1eed4c5a3e5b1322342fabeb
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-28 10:42:16 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,12 +1,10 @@
 
 #!/bin/bash
 set -e
-
-# Set your values
-REGION: /app/ecr-creds/region
-ACCOUNT_ID: /app/docker-creds/account-id
-REPO_NAME: /app/ecr-creds/IMAGE_REPO_NAME
-IMAGE_TAG: /app/docker-creds/image-tag
+REGION=$(aws ssm get-parameter --name "/app/ecr-creds/region" --query "Parameter.Value" --output text)
+ACCOUNT_ID=$(aws ssm get-parameter --name "/app/docker-creds/account-id" --query "Parameter.Value" --output text)
+REPO_NAME=$(aws ssm get-parameter --name "/app/ecr-creds/IMAGE_REPO_NAME" --query "Parameter.Value" --output text)
+IMAGE_TAG=$(aws ssm get-parameter --name "/app/docker-creds/image-tag" --query "Parameter.Value" --output text)
 
 echo "Logging into Amazon ECR..."
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com"
@@ -15,4 +13,4 @@ echo "Pulling the Docker image..."
 docker pull "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
 
 echo "Running the Docker container..."
-docker run -d -p 5000:5000 "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
+docker run -d -p 80:5000 "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
